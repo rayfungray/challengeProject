f645d285fee60f1162ac7cee426bca39836b3fb3
Ray <ray@Ray.local>
2021-09-23 15:14:24 -0600

    upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var whiteBox = document.getElementsByClassName("text-with-box");
+
+
+document.querySelector('.hit-detail').addEventListener("hover",function(){
+    console.log('test');
+    alert('hi')
+})
