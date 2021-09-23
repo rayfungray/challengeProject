4ed1fabe50e3e082603f0874d20196e487fe4e0b
Ray <ray@Ray.local>
2021-09-23 16:16:37 -0600

    fix text box

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var whiteBox = document.getElementsByClassName("text-with-box");
+var text = document.getElementsByClassName("text");
 
 
-document.querySelector('.hit-detail').addEventListener("hover",function(){
-    console.log('test');
-    alert('hi')
+document.querySelector('.hit-detail').addEventListener('click',function(){
+    whiteBox[0].classList.add('active');
+    text[0].classList.add('close');
+})
+
+document.querySelector('.close-btn').addEventListener('click',function(){
+    whiteBox[0].classList.remove('active');
+    text[0].classList.remove('close');
 })
